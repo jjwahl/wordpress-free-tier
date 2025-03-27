f97cbd12fe75f4cd30ca9fcd69187fdec11b755b
Jonas Wahl <jonasjorge97@gmail.com>
2025-03-27 02:19:33 +0000

    Fixed other issues v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,6 @@ resource "aws_db_instance" "wordpress_db" {
   password = var.db_password
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot = true
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 }
 
 
