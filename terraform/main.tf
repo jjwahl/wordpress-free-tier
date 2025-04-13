bfcb3e63a69a44f31ca54e29d2575eca512eb9d0
Jonas Wahl <jonasjorge97@gmail.com>
2025-04-14 00:23:07 +0100

    Settingup wordpress

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,15 @@ resource "aws_db_subnet_group" "default" {
 
 
 resource "aws_db_instance" "wordpress_db" {
+  vpc_security_group_ids = [aws_security_group.rds.id]
   db_subnet_group_name = aws_db_subnet_group.default.name
   allocated_storage  = 20
   engine = "mysql"
   engine_version = "8.0.35"
   instance_class = "db.t3.micro"
+  db_name              = var.db_name
+  username             = var.db_user
+  password             = var.db_password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot = true
   username = "admin"
@@ -53,6 +57,25 @@ resource "aws_security_group" "web" {
   }
 }
 
+resource "aws_security_group" "rds" {
+  name        = "wordpress-rds"
+  description = "Allow MySQL from EC2"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]  # for EC2!
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
   alarm_name = "wordpress-high-cpu"
   comparison_operator = "GreaterThanThreshold"
@@ -72,5 +95,19 @@ resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
 
 
 variable "db_password" {
-  sensitive = true
+  description = "RDS database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "WordPress database name"
+  type        = string
+  default     = "wordpress_db"
+}
+
+variable "db_user" {
+  description = "WordPress database user"
+  type        = string
+  default     = "admin"
 }
--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -8,3 +8,14 @@ wget https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz
 cp -r wordpress/* /var/www/html/
 chown -R apache:apache /var/www/html
+cd /var/www/html/
+cp wp-config-sample.php wp-config.php
+sed -i "s/database_name_here/wordpress_db/" wp-config.php
+sed -i "s/username_here/admin/" wp-config.php
+sed -i "s/password_here/${db_password}/" wp-config.php
+sed -i "s/localhost/${aws_db_instance.wordpress_db.endpoint}/" wp-config.php
+chmod 640 wp-config.php
+find /var/www/html/ -type d -exec chmod 755 {} \;
+find /var/www/html/ -type f -exec chmod 644 {} \;
+a2enmod rewrite
+systemctl restart apache2
