b032ca9e05e89674643266f2148c97282621a16e
Jonas Wahl <jonasjorge97@gmail.com>
2025-04-14 01:55:48 +0100

    testing connection errors

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -1,33 +1,8 @@
-#!/bin/bash
-yum update -y
-amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-yum install -y httpd mariadb-server
-systemctl start httpd
-systemctl enable httpd
-
-# Install WordPress
-wget https://wordpress.org/latest.tar.gz
-tar -xzf latest.tar.gz
-cp -r wordpress/* /var/www/html/
-rm -rf wordpress latest.tar.gz
-
-
-cd /var/www/html/
-
-
-cat > wp-config.php << EOL
-<?php
-define('DB_NAME', '${db_name}');
-define('DB_USER', '${db_user}');
-define('DB_PASSWORD', '${db_password}');
-define('DB_HOST', '${db_endpoint}');
-/* ... rest of wp-config.php contents ... */
-EOL
-
-# Set permissions
-chown -R apache:apache /var/www/html/
-find /var/www/html/ -type d -exec chmod 755 {} \;
-find /var/www/html/ -type f -exec chmod 644 {} \;
-
-echo "AllowOverride All" >> /etc/httpd/conf/httpd.conf
-systemctl restart httpd
+user_data = <<-EOF
+  #!/bin/bash
+  yum update -y
+  yum install -y httpd
+  systemctl start httpd
+  systemctl enable httpd
+  echo "Hello World" > /var/www/html/index.html
+EOF
